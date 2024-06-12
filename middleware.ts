f7c4f3eef1f6ea9c368b6fed6a9981f32c8dba1d
CAPITAN-GTG <138900207+CAPITAN-GTG@users.noreply.github.com>
2024-06-11 17:42:36 -0700

    fixed import command

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher([
+  '/transformations(.*)',
+  '/profile',
+  '/',
+]);
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
