4a7bd558099a8cacc53677a43469ff36b4c57286
CAPITAN-GTG <138900207+CAPITAN-GTG@users.noreply.github.com>
2024-06-14 19:59:40 -0700

    fixed json

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,35 +2,40 @@ import Header from '@/components/shared/Header'
 import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants'
 import { getUserById } from '@/lib/actions/user.actions';
-import { auth } from '@clerk/nextjs/server';
+import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import { Transform } from 'stream';
 
-const AddTransformationPage = async ({ params: { type } }:
-  SearchParamProps) => {
-    const {userId} = auth();
-    const transformation = transformationTypes[type];
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
 
-    if(!userId) redirect('/sign-in')
+  if(!userId) redirect('/sign-in')
 
-    const user = await getUserById(userId);
+console.log('before userId')
 
-    return (
-      <>
-        <Header
-          title={transformation.title}
-          subtitle={transformation.subTitle}
-          />
+  const user = await getUserById(userId);
 
-          <TransformationForm 
+  console.log(user)
+  console.log('hello world')
+
+  return (
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    
+      <section className="mt-10">
+        
+      </section>
+      <TransformationForm 
           action="Add"
           userId={user._id}
-          type={transformation.type as 
-          TransformationTypeKey}
+          type={transformation.type as TransformationTypeKey}
           creditBalance={user.creditBalance}
-          />
-        </>
-    )
-  }
+        />
+    </>
+  )
+}
 
-export default AddTransformationPage
+export default AddTransformationTypePage
